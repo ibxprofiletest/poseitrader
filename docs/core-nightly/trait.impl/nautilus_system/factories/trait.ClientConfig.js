f8207ad6c95cc2fb4762e01e887d285ce0a193b4
poseitrader <posei.dawn@outlook.com>
2025-07-08 07:45:06 +0800

    feat: initial commit with basic project structure

---
+++ docs/core-nightly/trait.impl/nautilus_system/factories/trait.ClientConfig.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["nautilus_blockchain",[["impl ClientConfig for <a class=\"struct\" href=\"nautilus_blockchain/config/struct.BlockchainDataClientConfig.html\" title=\"struct nautilus_blockchain::config::BlockchainDataClientConfig\">BlockchainDataClientConfig</a>"]]],["nautilus_databento",[["impl ClientConfig for <a class=\"struct\" href=\"nautilus_databento/factories/struct.DatabentoLiveClientConfig.html\" title=\"struct nautilus_databento::factories::DatabentoLiveClientConfig\">DatabentoLiveClientConfig</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[250,251]}
