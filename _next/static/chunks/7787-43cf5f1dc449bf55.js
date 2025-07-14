d3c2a8ed1229cab55a23b09a79e3b68d38b659f9
Ibxdevgh <ibxprofiletest@gmail.com>
2025-07-14 10:31:14 +0300

    update footer logo

--- _next/static/chunks/7787-43cf5f1dc449bf55.js
+++ _next/static/chunks/7787-43cf5f1dc449bf55.js
@@ -282,8 +282,8 @@
                       (0, n.jsx)(l.default, {
                         src: "/nautilus-logo-white.png",
                         alt: "footer-logo",
-                        width: 100,
-                        height: 100,
+                        width: 70,
+                        height: 70,
                         priority: !0,
                       }),
                       (0, n.jsxs)(i.default, {
